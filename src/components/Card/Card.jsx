b52ec3d2ea4adeb2472332159d015b2498fb31e9
CorentinDWWM <newsonic62@gmail.com>
2024-07-15 15:41:32 +0200

    push

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import styles from "./Card.module.scss";
 import { CardContext } from "../../context/CardContext";
-import carte from "../../assets/img/LasVegas.jpg";
+import carte from "../../assets/img/LasVegas2.jpg";
 
 export default function Card({ card, isFlipped, isMatched }) {
   const { handleCardClick } = useContext(CardContext);
