0353a5821fd71525e4222a759bcd13bfa59f76c8
CorentinDWWM <newsonic62@gmail.com>
2024-07-16 09:51:57 +0200

    push

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,20 @@
 import { Link, useLocation } from "react-router-dom";
 import styles from "./Header.module.scss";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { CardContext } from "../../context/CardContext";
+import HeaderMobile from "./components/HeaderMobile";
 
 function Header() {
   const { resetGame, score } = useContext(CardContext);
   const location = useLocation();
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <header className={`${styles.header} d-flex flex-row align-items-center`}>
       <div className={`d-flex flex-fill center ${styles.divScore}`}>
         {location.pathname === "/game" ? <p>{score}</p> : null}
       </div>
-      <div className="d-flex flex-rr">
+      <div className={`${styles.responsive} d-flex flex-rr`}>
         <div>
           <Link to="/" onClick={resetGame}>
             Accueil
@@ -23,6 +26,15 @@ function Header() {
           ) : null}
         </div>
       </div>
+      <i
+        onClick={() => setShowMenu(true)}
+        className={`fas fa-bars mr-10 ${styles.mobileHeader}`}
+      ></i>
+      {showMenu && (
+        <>
+          <HeaderMobile setShowMenu={setShowMenu} />
+        </>
+      )}
     </header>
   );
 }
---
+++ src/components/Header/components/HeaderMobile.jsx
@@ -0,0 +1,38 @@
+import { NavLink, useLocation } from "react-router-dom";
+import styles from "./HeaderMobile.module.scss";
+import { CardContext } from "../../../context/CardContext";
+import { useContext } from "react";
+
+export default function HeaderMobile({ setShowMenu }) {
+  const { resetGame } = useContext(CardContext);
+  const location = useLocation();
+
+  const handleClick = () => {
+    setShowMenu(false);
+    resetGame();
+  };
+
+  return (
+    <>
+      <ul className={`d-flex flex-column p-20 ${styles.container}`}>
+        <i
+          style={{ cursor: "pointer" }}
+          onClick={() => setShowMenu(false)}
+          className=" d-flex flex-column align-items-end fa-solid fa-xmark"
+        ></i>
+        <NavLink
+          onClick={handleClick}
+          to="/"
+          className={`${styles.btnNav} mt-10`}
+        >
+          Accueil
+        </NavLink>
+        {location.pathname === "/game" ? (
+          <a className={`${styles.btnNav}`} onClick={handleClick}>
+            Recommencer
+          </a>
+        ) : null}
+      </ul>
+    </>
+  );
+}
